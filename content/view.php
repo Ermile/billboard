b8c9b968e6e9b59299bb135c315b1a37db222d65
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-07-03 00:40:39 +0430

    use new homepage

--- content/view.php
+++ content/view.php
@@ -6,12 +6,10 @@ class view
 	public static function config()
 	{
 		// define default value for global
-		\dash\data::site_title(T_("Dash"));
-		\dash\data::site_desc(T_("Free PHP Framework & CMS!"));
-		\dash\data::site_slogan(T_("The simple framework for php programmers ;)"));
+		\dash\data::site_title(T_("Ermile"));
+		\dash\data::site_desc(T_("Software Solution Designer."));
+		\dash\data::site_slogan(T_("As easy as ABC is our slogan!"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
-		\dash\data::include_css(false);
-		\dash\data::bodyclass(null);
 	}
 }
 ?>
