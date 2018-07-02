f7a601a141f6f6f907be12e2c8fb0afceb298903
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-07-03 01:06:50 +0430

    fix page title

--- content/view.php
+++ content/view.php
@@ -9,6 +9,8 @@ class view
 		\dash\data::site_title(T_("Ermile"));
 		\dash\data::site_desc(T_("Software Solution Designer."));
 		\dash\data::site_slogan(T_("As easy as ABC is our slogan!"));
+		\dash\data::page_title(T_('Ermile Online Billboard'));
+		\dash\data::page_special(true);
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
 	}
 }
