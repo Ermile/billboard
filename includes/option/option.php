bf7ed2ffdfa973e18f7d015258970cb9d7a04b99
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-07-03 00:25:51 +0430

    add some files

---
+++ includes/option/option.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * save logs in other database
+ */
+
+/**
+@ In the name Of Allah
+* The base configurations of the dashsample.
+*/
+self::$language =
+[
+	'default' => 'fa',
+	'list'    => ['fa','en',],
+];
+
+?>
