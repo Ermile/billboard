e3ac27376acd527c4e6aa23b957445850aaf8e98
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-07-03 01:19:42 +0430

    update trans

---
+++ includes/languages/trans_project.php
@@ -0,0 +1,42 @@
+<?php
+class twigTransTerms
+{
+ private function transtext()
+ {
+
+	//-----------------------xampp\htdocs\projects\billboard\content\home\display.html
+	echo T_("Jibres");                                                                // Line 14
+	echo T_("Integrated Sales and Online Accounting");                                // Line 15
+	echo T_("Jibres is not just an online accounting software;");                     // Line 17
+	echo T_("We try to create the best financial platform that has everything you need to sale and manage your financial life.");// Line 17
+	echo T_("Azvir");                                                                 // Line 22
+	echo T_("Pioneer Academies Selection");                                           // Line 23
+	echo T_("Azvir provides complete and beautiful solutions for your academy;");     // Line 25
+	echo T_("Do all things easily and enjoy.");                                       // Line 25
+	echo T_("Sarshomar");                                                             // Line 30
+	echo T_("Ask Anyone Anywhere");                                                   // Line 31
+	echo T_("Focus on your question. Do not be too concerned about how to ask or analyze.");// Line 33
+	echo T_("Equipped with an integrated platform, Sarshomar has made it possible for you to ask your questions via any means.");// Line 33
+	echo T_("Tejarak");                                                               // Line 39
+	echo T_("Modern Approach");                                                       // Line 40
+	echo T_("Tejarak provides beautiful solutions for your business;");               // Line 42
+	echo T_("Do attendance easily and enjoy realtime reports.");                      // Line 42
+
+	//------------xampp\htdocs\projects\billboard\includes\languages\trans_static.html
+	echo T_("Dash");                                                                  // Line 7 Seperate
+
+	//xampp\htdocs\projects\billboard\public_html\static\siftal\fonts\siftal\icons-reference.html
+
+	//---xampp\htdocs\projects\billboard\public_html\static\siftal\js\amcharts3.min.js
+
+	//---xampp\htdocs\projects\billboard\public_html\static\siftal\js\amcharts4.min.js
+
+	//------xampp\htdocs\projects\billboard\public_html\static\siftal\js\error_page.js
+
+	//---xampp\htdocs\projects\billboard\public_html\static\siftal\js\highlight.min.js
+
+	//------xampp\htdocs\projects\billboard\public_html\static\siftal\js\siftal.min.js
+
+ }
+}
+?>
